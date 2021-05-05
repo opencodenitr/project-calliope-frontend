e7261d9654058de0ce65352c5aaa7b2abfc9e7e9
CIPHERTron <pritish.samal918@gmail.com>
2021-05-06 03:37:40 +0530

    fix: include navbar and footer inside Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Homepage, TeamPage } from "./pages";
-import { Navbar, Footer } from "./shared";
+import { Homepage, TeamPage } from './pages';
+import { Navbar, Footer } from './shared';
 
 function App() {
-	return (
-		<div className='App'>
-			<Navbar />
-			<Router>
-				<Switch>
-					<Route exact path='/' component={Homepage} />
-					<Route exact path='/team' component={TeamPage} />
-				</Switch>
-			</Router>
-			<Footer />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/team' component={TeamPage} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
