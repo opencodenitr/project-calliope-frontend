b1b8b768cb95e455e1894d52001aa6e3689a604e
CIPHERTron <pritish.samal918@gmail.com>
2021-05-06 20:57:59 +0530

    feat: Add Landing Page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import { Container } from '@material-ui/core';
+
+import background from '../assets/images/landing.svg';
+
+const Wrapper = styled(Container)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function Landing() {
-	return (
-		<div>
-			<h1>Welcome to D361</h1>
-		</div>
-	);
+  return (
+    <Wrapper maxWidth='md'>
+      <img src={background} alt='Landing Background' />
+    </Wrapper>
+  );
 }
 
 export default Landing;
