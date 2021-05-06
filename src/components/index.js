cc3102b5a5f1805313bbdbcc2ed3af982c93461b
CIPHERTron <pritish.samal918@gmail.com>
2021-05-06 21:02:00 +0530

    feat: Add Sample Footer Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Landing } from './Landing';
 export { default as TeamMember } from './TeamMember';
+export { default as RecentArticles } from './RecentArticles';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { FontStyles } from './themes/StyledComponents';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    <FontStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
