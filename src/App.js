64f92002f773f37e57a5bf855a5625ca283d3357
Anshuman Sandhibigraha <59245156+anshusandhi6@users.noreply.github.com>
2021-05-08 21:53:38 +0530

    Editors Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import { Homepage } from './pages';
-import MainTeam from "./pages/TeamPage"
+import MainTeam from "./pages/TeamPage";
+import Editors from "./pages/EditorsPage"
 import { Navbar, Footer } from './shared';
 
 function App() {
@@ -13,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route exact path='/team' component={MainTeam} />
+          <Route exact path='/Editors' component={Editors} />
         </Switch>
         <Footer />
       </Router>
