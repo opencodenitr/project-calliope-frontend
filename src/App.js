83503cc83da292be26324dd805547ceea2328f11
CIPHERTron <pritish.samal918@gmail.com>
2021-05-13 23:54:45 +0530

    refactor: Auth Check before rendering component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Homepage, TeamPage, EditorsPage } from './pages';
 import { Navbar, Footer } from './shared';
+import Auth from './utils/auth';
 
 function App() {
   return (
@@ -9,9 +10,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route exact path='/team' component={TeamPage} />
-          <Route exact path='/Editors' component={EditorsPage} />
+          <Route exact path='/' component={Auth(Homepage, null)} />
+          <Route exact path='/team' component={Auth(TeamPage, null)} />
+          <Route exact path='/Editors' component={Auth(EditorsPage, null)} />
         </Switch>
         <Footer />
       </Router>
