6775579c8face43a02e2d3bd209b53b7aca3c525
CIPHERTron <pritish.samal918@gmail.com>
2021-07-19 17:08:37 +0530

    feat: Add Legacy Dropdown Component

---
+++ components/dropdown.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Link from 'next/link';
+import { Typography, Menu, MenuItem } from '@material-ui/core';
+
+export default function Dropdown() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Typography
+        style={{ cursor: 'pointer' }}
+        aria-controls='simple-menu'
+        aria-haspopup='true'
+        onClick={handleClick}
+      >
+        Legacy
+      </Typography>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link href='/fan-fiction'>
+            <a style={{ textDecoration: 'none' }}>Fac Fiction</a>
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href='/sundry'>
+            <a style={{ textDecoration: 'none' }}>Sundry</a>
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href='/scribbles'>
+            <a style={{ textDecoration: 'none' }}>Scribbles</a>
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link href='/editors-desk'>
+            <a style={{ textDecoration: 'none' }}>Editor's Desk</a>
+          </Link>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
